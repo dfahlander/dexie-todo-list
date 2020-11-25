30ba8a3cfc989c3b226cca52cbefc02985aff905
dfahlander <david.fahlander@gmail.com>
2020-11-25 14:24:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dexie-todo-list
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dexie-todo-list)
---
+++ index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+import { AddTodoList } from "./components/AddTodoList";
+import { ResetDatabaseButton } from "./components/ResetDatabaseButton";
+import { TodoLists } from "./components/TodoLists";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <TodoLists />
+      <AddTodoList />
+      <ResetDatabaseButton />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
